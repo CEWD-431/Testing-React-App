ab0443acaa7a91f6d8d12c8500f28896f5511fd4
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-03-21 17:09:40 -0400

    define a test suite

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,17 +1,29 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
 
-test('renders Hello World as a text', () => {
+describe("Greeting Component", () => {
 
-    //Arrange
-    render(<Greeting />);
+    test('renders Hello World as a text', () => {
+        //Arrange
+        render(<Greeting />);
+        //Act 
+        // ....nothing
+        //Assert
+        const helloWorldElement = screen.getByText('Hello World!');
+        expect(helloWorldElement).toBeInTheDocument();
+    });
 
-    //Act 
-    // ....nothing
+    test('renders Hello World as a text', () => {
+        //Arrange
+        render(<Greeting />);
+        //Act 
+        // ....nothing
+        //Assert
+        const helloWorldElement = screen.queryByText('Hello World');
+        expect(helloWorldElement).not.toBeInTheDocument();
+    });
 
-    //Assert
-    const helloWorldElement = screen.getByText('Hello World!');
-    expect(helloWorldElement).toBeInTheDocument();
+});
 
-})
+
 
