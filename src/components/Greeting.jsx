c0e83bfc36422819727724d0983121c98c3b20c2
KritikaSharmaKS <sh.kritika01@gmail.com>
2024-03-21 16:49:39 -0400

    add Greeting component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,8 @@
+export default Greeting = () => {
+    return (
+        <div>
+            <h2>Hello World!</h2>
+            <p>It is so good to see you... Welcome, we test our stuff here!</p>
+        </div>
+    )
+}
